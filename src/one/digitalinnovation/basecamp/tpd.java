4210a131909d07981439a6102a28c98d9cbb4f18
Juan Felipe da Silva <juan.felipedssantos@gmail.com>
2022-01-07 23:16:38 -0300

    Commit tipos de dados

---
+++ src/one/digitalinnovation/basecamp/tpd.java
@@ -0,0 +1,56 @@
+package one.digitalinnovation.basecamp;
+
+public class tpd {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+//        short s2 = 40000;
+
+//        int i1 = -10000000000;
+        int i2 = 28500;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 2413548465184513284L;
+
+//        float f1 = 4.5;
+        float f2 = 2.68f;
+
+        double d1 = 10.12;
+        double d2 = 12.58d;
+
+        char c1 = 'W';
+//        char c2 = 'tW';
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+        String st3 = "askhkadsn 321343";
+
+        String dt1 = "12/11/2001";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(dt1);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+    }
+}
